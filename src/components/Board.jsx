98e184c54de62f94c6a6cf63a2deac820c4ebdaf
Fabrice Defay <defay.fabrice@gmail.com>
2021-10-20 10:39:20 -0400

    changes board to flat

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 export default function Board() {
-  const [board, setBoard] = useState(Array(10).fill(Array(10).fill(null)));
   const piece = {
     name: "knight",
     img: "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
+    value: null,
   };
+  const [board, setBoard] = useState(Array(10).fill(Array(10).fill(piece)));
 
-  function placeKnigth(i, j) {
-    const board = Array(10).fill(Array(10).fill(0));
-    board[i][j] = piece;
+  function placeKnight(e, i, j) {
+    console.log(e.target.value);
+    const board = Array(10).fill(Array(10).fill(piece));
+    board[i][j]["value"] = `${i}-${j}`;
     console.log(board);
     setBoard(board);
   }
@@ -18,12 +20,28 @@ export default function Board() {
     return row.map((square, j) => {
       const name = (i + j) % 2 === 0 ? "black" : "red";
       return (
-        <div key={i + j} className={name} onClick={(e) => placeKnigth(i, j)}>
-          {square && <img key={j} src={square.img} alt={square.name} />}
+        <div key={i + j} className={name} onClick={(e) => placeKnight(e, i, j)}>
+          {square.value && <img src={square.img} alt={square.name} />}
         </div>
       );
     });
   });
+  console.log(rows[0][0]);
+  const flatBoard = rows.flat();
+  console.log(flatBoard);
 
-  return <div className="board">{rows.map((row) => row)}</div>;
+  return (
+    <div className="board">
+      {flatBoard.map((square) => square)}
+      {/*<div>{rows[1].map((row) => row)}</div>
+      <div>{rows[2].map((row) => row)}</div>
+      <div>{rows[3].map((row) => row)}</div>
+      <div>{rows[4].map((row) => row)}</div>
+      <div>{rows[5].map((row) => row)}</div>
+      <div>{rows[6].map((row) => row)}</div>
+      <div>{rows[7].map((row) => row)}</div>
+      <div>{rows[8].map((row) => row)}</div>
+  <div>{rows[9].map((row) => row)}</div>*/}
+    </div>
+  );
 }
