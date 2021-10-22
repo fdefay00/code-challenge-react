e963ee93b8ce64a551b087cdf5e7bba8e599f2e0
Fabrice Defay <defay.fabrice@gmail.com>
2021-10-21 21:32:45 -0400

    initial logic for block and unblock

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,14 +6,19 @@ export default function Board() {
     img: "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png",
     value: null,
   };
-  const [board, setBoard] = useState(Array(10).fill(Array(10).fill(null)));
+  const [board, setBoard] = useState(createBoard(10));
   const [currentPosition, setCurrentPosition] = useState();
+  const [currentBlocks, setCurrentBlocks] = useState([]);
+  const [openMoves, setOpenMoves] = useState([]);
   const [moving, setMoving] = useState(false);
   const [blocking, setBlocking] = useState(false);
   const [cancelMove, setCancelMove] = useState(false);
   const [cancelBlock, setCancelBlock] = useState(false);
+  const img = <img src={piece.img} alt={piece.name} />;
+  const size = 10;
 
   function handleClick(i, j, e) {
+    if (board[i][j]?.type === "img") return showAvailableSpot(i, j, e);
     if (moving) {
       if (board[i][j] === "Ⓜ️") moveToChosenSpot(i, j, e);
       else {
@@ -25,49 +30,57 @@ export default function Board() {
     }
 
     if (board[i][j] === null) return placeKnight(i, j);
-    if (board[i][j].type === "img") showAvailableSpot(i, j, e);
+  }
+
+  function createBoard(size) {
+    const board = [];
+    for (let i = 0; i < size; i++) {
+      board[i] = Array(10);
+      for (let j = 0; j < size; j++) {
+        board[i][j] = null;
+      }
+    }
+    console.log(board);
+    return board;
   }
 
   function placeKnight(i, j) {
     setCurrentPosition([i, j]);
-    const board = Array(10).fill(Array(10).fill(null));
-    const img = <img src={piece.img} alt={piece.name} />;
+    const board = createBoard(10);
     drawBoard(i, j, img, board);
     setBoard(board);
   }
 
   function drawBoard(l, k, value, board) {
-    for (let i = 0; i < 10; i++) {
-      board[i] = [];
-      for (let j = 0; j < 10; j++) {
-        if (i === l && j === k) board[i].push(value);
-        else board[i].push(null);
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        if (i === l && j === k) {
+          board[i][j] = value;
+        }
       }
     }
-    // console.log({ board });
     return board;
   }
 
-  function showAvailableSpot(k, l) {
+  function showAvailableSpot() {
     setMoving(true);
-    const board = [];
+    const board = createBoard(size);
 
-    for (let i = 0; i < 10; i++) {
-      board[i] = [];
-      for (let j = 0; j < 10; j++) {
-        if (i === k && j === l) board[i].push(<img src={piece.img} alt={piece.name} />);
-        else if (i === k + 2 && j === l + 1) board[i].push("Ⓜ️");
-        else if (i === k + 2 && j === l - 1) board[i].push("Ⓜ️");
-        else if (i === k - 2 && j === l + 1) board[i].push("Ⓜ️");
-        else if (i === k - 2 && j === l - 1) board[i].push("Ⓜ️");
-        else if (i === k + 1 && j === l + 2) board[i].push("Ⓜ️");
-        else if (i === k + 1 && j === l - 2) board[i].push("Ⓜ️");
-        else if (i === k - 1 && j === l + 2) board[i].push("Ⓜ️");
-        else if (i === k - 1 && j === l - 2) board[i].push("Ⓜ️");
-        else board[i].push(null);
-      }
-    }
-    // console.log(board);
+    const openMoves = [];
+    const i = currentPosition[0];
+    const j = currentPosition[1];
+    openMoves.push([i + 2, j + 1]);
+    openMoves.push([i + 2, j - 1]);
+    openMoves.push([i - 2, j + 1]);
+    openMoves.push([i - 2, j - 1]);
+    openMoves.push([i + 1, j + 2]);
+    openMoves.push([i + 1, j - 2]);
+    openMoves.push([i - 1, j + 2]);
+    openMoves.push([i - 1, j - 2]);
+    setOpenMoves(openMoves);
+    openMoves.forEach((position) => drawBoard(position[0], position[1], "Ⓜ️", board));
+    if (currentPosition) drawBoard(currentPosition[0], currentPosition[1], img, board);
+    currentBlocks.forEach((position) => drawBoard(position[0], position[1], "🔒", board));
     setBoard(board);
   }
 
@@ -77,14 +90,27 @@ export default function Board() {
   }
 
   function block(i, j) {
-    const board = [];
-    drawBoard(i, j, "🔒", board);
-    drawBoard(i + 1, j, "🔒", board);
-    drawBoard(i, j + 1, "🔒", board);
-    drawBoard(i - 1, j, "🔒", board);
-    drawBoard(i + 1, j + 1, "🔒", board);
-    drawBoard(i - 1, j - 1, "🔒", board);
+    const board = createBoard(size);
+    const currentBlocks = [];
+    currentBlocks.push([i, j]);
+    currentBlocks.push([i + 1, j]);
+    currentBlocks.push([i - 1, j]);
+    currentBlocks.push([i, j + 1]);
+    currentBlocks.push([i, j - 1]);
+    currentBlocks.push([i + 1, j + 1]);
+    currentBlocks.push([i + 1, j - 1]);
+    currentBlocks.push([i - 1, j + 1]);
+    currentBlocks.push([i - 1, j - 1]);
+    currentBlocks.forEach((position) => drawBoard(position[0], position[1], "🔒", board));
+    if (currentPosition) drawBoard(currentPosition[0], currentPosition[1], img, board);
+    setCurrentBlocks(currentBlocks);
     setBoard(board);
+  }
+
+  function unblock() {
+    setBlocking(false);
+    const board = createBoard(size);
+    if (currentPosition) drawBoard(currentPosition[0], currentPosition[1], img, board);
   }
 
   const rows = board.map((row, i) => {
@@ -97,30 +123,26 @@ export default function Board() {
       );
     });
   });
-  console.log("rows yeah", rows);
+  // console.log("rows yeah", rows);
+
+  const blockButton = blocking ? (
+    <button onClick={unblock}>UnBlock Knight</button>
+  ) : (
+    <button onClick={() => setBlocking(true)}>Block Knight</button>
+  );
 
   return (
     <>
       <div className="header">
         <h1>Game ON!</h1>
-        <button onClick={() => setBlocking(true)}>Block Knight</button>
+        {blockButton}
+        <button></button>
         {blocking && <h5>Choose area to block knight</h5>}
         {moving && <h5>Pick available spot to move piece</h5>}
         {cancelMove && <h5>Move canceled</h5>}
       </div>
       <div className="container">
-        <div className="board">
-          {rows[0].map((row) => row)}
-          {rows[1].map((row) => row)}
-          {rows[2].map((row) => row)}
-          {rows[3].map((row) => row)}
-          {rows[4].map((row) => row)}
-          {rows[5].map((row) => row)}
-          {rows[6].map((row) => row)}
-          {rows[7].map((row) => row)}
-          {rows[8].map((row) => row)}
-          {rows[9].map((row) => row)}
-        </div>
+        <div className="board">{rows.map((row) => row)}</div>
       </div>
     </>
   );
